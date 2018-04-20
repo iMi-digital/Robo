674b478b812b140732046ef10d0a8d41f0c02a6b
Alexander Menk <a.menk@imi.de>
2018-04-20 17:06:00 +0200

    Composer update after merge

--- src/Robo.php
+++ src/Robo.php
@@ -20,7 +20,7 @@ use Symfony\Component\Process\Process;
 class Robo
 {
     const APPLICATION_NAME = 'iRobo';
-    const VERSION = '2.4.4';
+    const VERSION = '2.5.0';
 	const ROBO_BASE_VERSION = '1.2.4-dev';
 
     /**
