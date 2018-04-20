624e33137e9000bb4a2dcb9544d905aed68e67f8
Alexander Menk <a.menk@imi.de>
2018-04-20 17:28:23 +0200

    Version bump

--- src/Robo.php
+++ src/Robo.php
@@ -20,7 +20,7 @@ use Symfony\Component\Process\Process;
 class Robo
 {
     const APPLICATION_NAME = 'iRobo';
-    const VERSION = '2.5.1';
+    const VERSION = '2.5.3';
 	const ROBO_BASE_VERSION = '1.2.4-dev';
 
     /**
